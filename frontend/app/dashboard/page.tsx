4d86328884d5a35eb174ce0f84402d04aaa59f9e
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2024-02-24 13:37:42 +0530

    added sidebar

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,4 +1,14 @@
+"use client"
+import Sidebar from "./sidebar";
+import { useState } from "react";
+
+interface SidebarProps{
+  sidebarOpen: boolean;
+  setSidebarOpen:(arg:boolean) => void;
+}
+
 const page = () => {
-  return <div>Dashboard</div>;
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  return <div>        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/></div>;
 };
 export default page;
